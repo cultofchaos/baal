66a1dc97c6ffb493aa05cf8fad07dca67bba4e3a
Eric <81354428+s0mt0chukwu@users.noreply.github.com>
2023-09-08 01:42:39 +0000

    initial commit

---
+++ src/index.php
@@ -0,0 +1,38 @@
+<?
+   include (‘include_fns.php’);
+   session_start();
+
+   // check if we have created our session variable
+   if(!session_is_registered(‘expanded’))
+   {
+      $expanded = array();
+      session_register(‘expanded’);
+    }
+
+    // check if an expand button was pressed
+    // expand might equal ‘all’ or a postid or not be set
+    if($expand)
+    {
+       if($expand == ‘all’)
+          expand_all($expanded);
+        else
+           $expanded[$expand] = true;
+    }
+
+    // check if a collapse button was pressed
+    // collapse might equal all or a postid or not be set
+    if($collapse)
+    {
+      if($collapse==”all”)
+      unset($expanded);
+    else
+      unset($expanded[$collapse]);
+    }
+    do_html_header(“Threads”);
+
+    display_index_toolbar();
+
+    // display the tree view of conversations
+    display_tree($expanded);
+    do_html_footer();
+?>
